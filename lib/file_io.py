be4f27b5ed2d09240080c531ad522748331c1f73
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-08-02 12:51:30 +0300

    completes tasks

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
 def write_file(file_name, file_content):
-    pass
+    # Ensure the file has a .txt extension
+    file_name = f"{file_name}.txt"
+    
+    # Open the file for writing with utf-8 encoding
+    with open(file_name, mode='w', encoding='utf-8') as text_file:
+        text_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, file_content):
+
+    file_name = f"{file_name}.txt"
+    
+    # Open the file for appending with utf-8 encoding
+    with open(file_name, mode='a', encoding='utf-8') as text_file:
+        text_file.write(file_content)
 
 def read_file(file_name):
-    pass
+
+    file_name = f"{file_name}.txt"
+    
+    # Open the file for reading with utf-8 encoding
+    with open(file_name, mode='r', encoding='utf-8') as text_file:
+        return text_file.read()
+
